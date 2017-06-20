c4c7e27f0b39fbad95797e4e93c6085ad823bfa6
azheng39 <azheng39@binghamton.edu>
2017-06-20 13:04:02 -0400

    Add files via upload

---
+++ lab7.py
@@ -0,0 +1,54 @@
+import pygame
+
+class Bunny:
+
+	def __init__(self):
+
+		self.jumptimes = 2 #times can jump w/o ground refresh
+		self.jumpup = False #is bunny allowed to jump
+		self.colliding = False #is bunny colliding with obstacle
+
+		self.x = 0 #sets position
+		self.y = 0	
+		self.velocity = 0
+		self.gravity = -10
+
+	def jump(self):
+
+		if (event.key == pygame.K_UP):
+			
+			self.jumpup = True
+		
+	def collide(self, enemy):
+
+
+
+	def draw(self,screen):
+		
+		if(self.jumpup):
+
+			self.velocity = 20
+			self.velocity += self.gravity
+			self.y += self.veolcity
+
+
+class Floor:
+
+	def __init__(self):
+
+		
+
+class Background:
+
+	def __init__(self):
+
+
+
+	def scroll(self):
+
+
+class Obstacle:
+
+	def __init__(self):
+
+
