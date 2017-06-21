3516e654090ef8d6d2e100a9a9e8de9380a69ee5
vexvictory <yfu23@binghamton.edu>
2017-06-22 02:35:58 +0800

    Update Background.py

--- Background.py
+++ Background.py
@@ -2,6 +2,15 @@ import pygame
 
 class Background():
 
-    def __init__(self, backgroundImage):
+    def __init__(self, backgroundImage,x,y):
 
-        self.backgroundImage = pygame.image.load(backgroundImage)
+        self.backgroundImage = pygame.image.load(backgroundImage).convert_alpha()
+        self.x = x
+        self.y = y
+    def scroll(self):
+        if self.x < -300:
+            self.x = 300
+        else:
+            self.x -=5
+    def update(self):
+        screen.blit(self.backgroundImage(self.x,self.y))
