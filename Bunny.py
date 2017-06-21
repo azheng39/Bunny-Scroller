9b8abaa1a52e6e585b4fc0574c5255eadf88594b
Allen Zheng <azheng39@binghamton.edu>
2017-06-21 09:38:30 -0400

    Update Bunny.py

--- Bunny.py
+++ Bunny.py
@@ -1,5 +1,35 @@
 import pygame
 
-class Bunny():
+class Bunny:
 
-    def __init__(self):
+	def __init__(self):
+
+		self.jumptimes = 2 #times can jump w/o ground refresh
+		self.jumpup = False #is bunny allowed to jump
+		self.colliding = False #is bunny colliding with obstacle
+
+		self.x = 0 #sets position
+		self.y = 0	
+		self.velocity = 0 #vertical velocity is 0
+		self.gravity = -10 #gravitational constant
+
+	def jump(self):
+
+		if (event.key == pygame.K_UP):
+			
+			self.jumpup = True
+		
+	def collide(self, enemy):
+
+		if(self.colliding):
+			
+			self.velocity = 0
+			print("You lost!")
+
+	def draw(self,screen):
+		
+		if(self.jumpup):
+
+			self.velocity = 20 #changes vert velocity to go up
+			self.velocity += self.gravity #decreases velocity 
+self.y += self.velocity #changes position of bunny 
