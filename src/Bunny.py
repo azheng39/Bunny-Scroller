3b9ff46c6f577ae0e9798d04958d72bf3c59a344
Allen Zheng <azheng39@binghamton.edu>
2017-06-22 10:25:38 -0400

    added update image frame function

--- src/Bunny.py
+++ src/Bunny.py
@@ -7,15 +7,18 @@ class Bunny():
         pygame.sprite.Sprite.__init__(self) #sprite init
         self.bunnyImage, self.rect = load_png('') #load bunny image
         self.rect = self.rect.inflate(VALUE, VALUE) #.RECT IT
+	self.counter = 10
+	self.imagecounter = 0
 
-		self.jumptimes = 2 #times can jump w/o ground refresh
-		self.jumpup = False #is bunny allowed to jump
-		self.colliding = False #is bunny colliding with obstacle
+	self.jumptimes = 2 #times can jump w/o ground refresh
+	self.jumpup = False #is bunny allowed to jump
+	self.colliding = False #is bunny colliding with obstacle
 
-		self.x = 0 #sets position
-		self.y = 0
-		self.velocity = 0 #vertical velocity is 0
-		self.gravity = -10 #gravitational constant
+	self.x = 0 #sets position
+	self.y = 0
+	self.velocity = 0 #vertical velocity is 0
+	self.gravity = -10 #gravitational constant
+	
         self.rect.x = 0
         self.rect.y = 0
 
@@ -27,6 +30,15 @@ class Bunny():
 	def collide(self, enemy):
         ''' Collision boolean '''
 		self.colliding = True
+
+    def update(self):
+        '''Cycles through bunny images'''
+        self.counter = self.counter + 1
+        self.bunnyImage = load_png("../assets/" + str(self.imageCounter) + ".png")
+        if self.counter % 10 == 0:
+		self.imageCounter = self.imageCounter + 1
+        if(self.imageCounter == 4):
+		self.imageCounter = 0
 
 	def draw(self,screen):
         '''  draws the bunny objects jump and collision '''
