efb47946ce4da91b07ed90162852e5b29317bb65
Allen Zheng <azheng39@binghamton.edu>
2017-06-26 10:26:00 -0400

    Add files via upload

--- src/bunny.py
+++ src/bunny.py
@@ -3,36 +3,32 @@ import pygame
 class Bunny():
 
 	def __init__(self):
-    	''' init bunny object '''
-        pygame.sprite.Sprite.__init__(self) #sprite init
-        self.bunnyImage, self.rect = load_png('') #load bunny image
-        self.rect = self.rect.inflate(VALUE, VALUE) #.RECT IT
-	self.counter = 10
-	self.imagecounter = 0
+		pygame.sprite.Sprite.__init__(self)
+		self.bunnyImage, self.rect = load_png('') #load bunny image
+		self.rect = self.rect.inflate(VALUE, VALUE) #.RECT IT
+		self.counter = 10
+		self.imagecounter = 0
 
-	self.jumptimes = 2 #times can jump w/o ground refresh
-	self.jumpup = False #is bunny allowed to jump
-	self.colliding = False #is bunny colliding with obstacle
+		self.jumptimes = 2 #times can jump w/o ground refresh
+		self.jumpup = False #is bunny allowed to jump
+		self.colliding = False #is bunny colliding with obstacle
 
-	self.x = 0 #sets position
-	self.y = 0
-	self.v = 8 #vertical velocity is 0
-	self.m = -2 #mass
+		self.x = 0 #sets position
+		self.y = 0
+		self.v = 8 #vertical velocity is 0
+		self.m = -2 #mass
 
-        self.rect.x = 0
-        self.rect.y = 0
+		self.rect.x = 0
+		self.rect.y = 0
 
 
 	def jump(self):
-        ''' Jump boolean '''
 		self.jumpup = True
 
 	def collide(self, enemy):
-        ''' Collision boolean '''
 		self.colliding = True
 
 	def update(self):
-        '''Cycles through bunny images'''
 		self.counter = self.counter + 1
 		self.bunnyImage = load_png("../assets/" + str(self.imageCounter) + ".png")
 		if self.counter % 10 == 0:
@@ -41,7 +37,6 @@ class Bunny():
 			self.imageCounter = 0
 
 	def draw(self,screen):
-        '''  draws the bunny objects jump and collision '''
 		if(self.jumpup):
 			if(self.v>0):
 				F = (0.5 * self.m * (self.v * self.v))
@@ -55,5 +50,6 @@ class Bunny():
 			self.jumpup = False
 			self.v = 8
 
-        if(self.colliding):
-                self.colliding = False
+		if(self.colliding):
+		        self.colliding = False
+		screen.blit(self.image, (self.rect.x, self.rect.y))
