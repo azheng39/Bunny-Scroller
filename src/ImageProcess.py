72179d792a526d232cdf1146fbd45a34fd348d98
CToprak <cantoprak20@yahoo.com>
2017-06-27 12:51:43 -0400

    fix load_img

--- src/ImageProcess.py
+++ src/ImageProcess.py
@@ -2,13 +2,13 @@ import os
 import sys
 import pygame
 
-def load_image(imageName):
+def load_image(name):
     ''' Take input of image file and return image object. '''
 
-    processingImage = pygame.image.load(imageName)
+    image = pygame.image.load(name)
     if image.get_alpha is None:
         image = image.convert()
     else:
         image = image.convert_alpha()
 
-    return processingImage, processingImage.get_rect()
+    return image, image.get_rect()
