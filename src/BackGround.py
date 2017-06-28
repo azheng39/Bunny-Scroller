8eeac413751cf7a674e9d53ad161577bd68d1371
CToprak <cantoprak20@yahoo.com>
2017-06-28 10:03:53 -0400

    quick background.py refactor

--- src/BackGround.py
+++ src/BackGround.py
@@ -3,9 +3,9 @@ import pygame
 class Background(pygame.sprite.Sprite):
 
     def __init__(self, name):
-        pygame.sprite.Sprite.__init__(self) #Call Sprite init
-
         ''' Load in the background image and .get_rect it '''
+
+        pygame.sprite.Sprite.__init__(self) #Call Sprite init
 
         try:
             self.image = pygame.image.load(name)
@@ -13,8 +13,9 @@ class Background(pygame.sprite.Sprite):
                 self.image = self.image.convert()
             else:
                 self.image = self.image.convert_alpha()
+                
         except pygame.error as message:
-            print('Cannot load iamge' + name)
+            print('Cannot load image' + name)
             raise SystemExit(message)
 
         self.rect = self.image.get_rect()
